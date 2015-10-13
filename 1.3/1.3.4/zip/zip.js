f71d73252e120d384afca008b265c6e5e503fef5
cangku <cangku@unofficial.cn>
2015-10-13 22:51:02 +0800

    优化

--- 1.3/1.3.4/zip/zip.js
+++ 1.3/1.3.4/zip/zip.js
@@ -7,11 +7,12 @@
  */
 var fs = require('fs'),
 	path = require('path'),
+	moment = require('moment'),
 	archiver = require('archiver'),
 	archive = archiver.create('zip'),
 	fileOrDirPath = process.argv.slice(2)[0],
 	fileOrDirName = path.basename(fileOrDirPath, path.extname(fileOrDirPath)),
-	fileOrDirName = fileOrDirName == '.' || fileOrDirName == '..' ? Math.floor(Math.random() * 1000) : fileOrDirName;
+	fileOrDirName = fileOrDirName == '.' || fileOrDirName == '..' ? moment().format('YYYYMMDDhhmmss') : fileOrDirName,//未命名文件或者文件夹（./或者../）以当前时间为准
 	output = fs.createWriteStream(fileOrDirName+'.zip');
 //判断是文件还是文件夹
 fs.stat(fileOrDirPath, function(err, stats) {
@@ -24,10 +25,8 @@ fs.stat(fileOrDirPath, function(err, stats) {
 	archive.pipe(output);
 	archive.finalize();
 })
-
 /**
- * 待完善 => 3
- * 未命名文件或者文件夹（./或者../）以当前时间为准，可以参考（moment.js）
+ * 待完善 => 2
  * ./或者../时排除压缩文件本身
  * 检测到同名压缩包存在，重命名压缩包文件
  */
