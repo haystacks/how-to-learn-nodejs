1c9d539a47f5c1aa4f9cfb66b68d400221acef30
cangku <cangku@unofficial.cn>
2015-12-01 10:11:20 +0800

    1.3.4 end new start

--- 1.3/1.3.4/work.js
+++ 1.3/1.3.4/work.js
@@ -1,3 +1,12 @@
 /**
- * 用于打补丁
+ * 实例讲解
+ * @param patch文件夹 目录结构按照压缩包的目录结构 PC1111/1111/项目根目录
+ * @param 模板文件夹 dest，遍历当前文件夹
  */
+
+var traversal = require('traversal')['traversal'];
+var fs = require('fs');
+var archiver = require('archiver');
+var archive = archiver.create('zip');
+archive.append(fs.createReadStream('a/a.zip'), { name:'traversal.js' });
+archive.pipe(fs.createWriteStream(zipPath));
