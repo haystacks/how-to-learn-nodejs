62110e2833824e58ee4704a950472aec427b81ba
cangku <cangku@unofficial.cn>
2015-12-08 17:59:52 +0800

    lazy

--- other/http/vue-weibo-photos/src/components/lazyloadimg.js
+++ other/http/vue-weibo-photos/src/components/lazyloadimg.js
@@ -1,4 +1,23 @@
 var lazy = function () {
     var images = document.images;
-}
+    for (obj of images) {
+        if(obj.getBoundingClientRect().top < document.documentElement.clientHeight && !obj.isLoad) {
+            obj.isLoad = true;
+            // 先调用 HTML5 方法
+            if (obj.dataset)
+                imageLoaded(obj, obj.dataset.original);
+            else
+                imageLoaded(obj, obj.getAttribute('data-original'));
+        } else {
+            break;
+        }
+    }
+};
+var img = new Image();
+var imageLoaded = function(obj, original) {
+    img.src = original;
+    img.onload = function() {
+        obj.src = original;
+    }
+};
 export default lazy;
