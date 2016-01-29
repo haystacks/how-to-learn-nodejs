db1836d6bbc7b20cec65bf56ce4fb4ea13d7461e
unofficial <cangku@unofficial.cn>
2016-01-29 17:30:06 +0800

    timeline

--- other/timeline/index.js
+++ other/timeline/index.js
@@ -2,22 +2,52 @@
  * 微博辩论时间线类
  */
 'use strict';
-let request = require('./request');
+let http = require('http');
+let url = require('url');
 class WeiBian {
     constructor(url) {
         this.x = 1;
         this.y = 2;
         this.url = url;
+        // parse the url
+        this.parseUrl();
     }
 
-    // 微博原文
-    content() {
-        let res = request.get(this.url);
+    //parse url
+    parseUrl() {
+        let urlJson = url.parse(this.url);
+        this.hostname = urlJson.hostname;
+        this.pathname = urlJson.pathname;
+    }
+
+    // request
+    request(param) {
+        let options = {
+            hostname: param.hostname,
+            path: param.path,
+            headers: {
+                'cookie':'SUB=_2AkMh9nhGdcPhrABZn_oXzG_naY5PiV6l55TvKlTsJxEzHhl_7W5l2lZrtRN-XNAPx3-H1Kl_YmFKOrjmorrXR_hn;'
+            }
+        };
+
+        let req = http.request(options, (res) => {
+            console.log(res.statusCode);
+            res.on('data', (chunk) => {
+                console.log(`BODY: ${chunk}`);
+            })
+            res.on('end', () => {
+                console.log('response end');
+            })
+        });
+
+        req.end();
     }
 
     // 转发
     repost() {
-        console.log(this.x, this.y);
+        let hostname = this.hostname;
+        let path = this.pathname + '?type=repost';
+        this.request({hostname: hostname, path: path});
     }
 
     // 评论
@@ -36,6 +66,6 @@ class WeiBian {
     }
 }
 
-var Wb = new WeiBian('http://weibo.com/1761511274/DeyVCxL9Q');
-Wb.test();
-Wb.content();
+var Wb = new WeiBian('http://weibo.com/1761511274/DeyVCxL9Q?type=comment');
+
+Wb.repost();
