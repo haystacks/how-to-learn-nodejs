8d48228487d76fe8fe4cd895db767dc3cde78d8f
unofficial <cangku@unofficial.cn>
2017-03-13 06:03:19 +0800

    update

--- login/taobao/test.js
+++ login/taobao/test.js
@@ -1,6 +1,29 @@
-var child = require('child_process');
-var s = child.execSync('./phantomjs baidu.js');
+// var child = require('child_process');
+// var s = child.execSync('./phantomjs baidu.js');
 
-console.log(s);
+// console.log(s);
 // https://github.com/amir20/phantomjs-node
 // https://github.com/casperjs/casperjs
+const phantom = require('phantom');
+
+var rs = (async function() {
+    const instance = await phantom.create();
+    const page = await instance.createPage();
+    await page.on("onResourceRequested", function(requestData) {
+        console.info('Requesting', requestData.url)
+    });
+
+    const status = await page.open('https://stackoverflow.com/');
+    console.log(status);
+
+    const content = await page.property('content');
+    // console.log(content);
+
+    await instance.exit();
+    return status;
+}());
+
+rs.then(function(e) {
+    console.log(e);
+})
+
