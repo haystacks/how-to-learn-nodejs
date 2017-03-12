2bef224692647274aa2ac8e777f3ff7af02ab7b6
unofficial <cangku@unofficial.cn>
2017-03-12 21:34:50 +0800

    update

--- login/taobao/test.js
+++ login/taobao/test.js
@@ -2,3 +2,5 @@ var child = require('child_process');
 var s = child.execSync('./phantomjs baidu.js');
 
 console.log(s);
+// https://github.com/amir20/phantomjs-node
+// https://github.com/casperjs/casperjs
