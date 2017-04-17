a5a012ee89c12dff677d9a0a04be8066b000e71b
unofficial <cangku@unofficial.cn>
2017-04-18 07:09:19 +0800

    update

--- login/taobao/phantom.js
+++ login/taobao/phantom.js
@@ -1,7 +1,7 @@
 var fs     = require('fs');
 var page   = require('webpage').create();
 
-page.settings.userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1';
+page.settings.userAgent = 'Mozilla/5.0 (Linux; Android 5.1; m2 note Build/LMY47D; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/53.0.2785.49 Mobile MQQBrowser/6.2 TBS/043024 Safari/537.36 MicroMessenger/6.5.4.1000 NetType/WIFI Language/zh_CN';
 page.onConsoleMessage = function(msg) {
     console.log(msg);
 }
@@ -15,11 +15,12 @@ page.onResourceRequested = function(requestData, networkRequest) {
         console.log('Request (#' + requestData.url + ')');
 };
 page.onResourceReceived = function(response) {
+    console.log(response.url);
     if(response.url === 'https://login.m.taobao.com/login.htm?_input_charset=utf-8') {
-        console.log('Response (#' + response.id + ', stage "' + response.stage + '"): ' + JSON.stringify(response));
+        //console.log('Response (#' + response.id + ', stage "' + response.stage + '"): ' + JSON.stringify(response));
         page.render('./taobaolog/taobao.jpeg', {format: 'jpeg', quality: '100'});
     } else if(response.url === 'http://www.alimama.com/index.htm') {
-        console.log(JSON.stringify(response), JSON.stringify(page.cookies));
+        //console.log(JSON.stringify(response), JSON.stringify(page.cookies));
         page.render('./taobaolog/alimama.jpeg', {format: 'jpeg', quality: '100'});
     }
 };
@@ -46,22 +47,32 @@ page.open('https://login.m.taobao.com/login.htm?redirectURL=http://www.alimama.c
                 el.dispatchEvent(event);
             }
 
-            function focus(el) {
+            function ifocus(el) {
                 var event = new Event('focus');
+                el.dispatchEvent(event);
+            }
+
+            function iblur(el) {
+                var event = new Event('blur');
                 el.dispatchEvent(event);
             }
 
             setTimeout(function() {
                 //touchstart(document, 188, 306, i++, document.getElementById('username'));
-                focus($('#username')[0]);
-                $('#username').val('');
+                ifocus($('#username')[0]);
+                $('#username').val(config.username);
+                $('#username').trigger('keyup');
                 $('#username').trigger('input');
+                iblur($('#username')[0]);
             }, 3e3)
             setTimeout(function() {
                 //touchstart(document, 188, 402, i++, document.getElementById('password'));
-                focus($('#password')[0]);
-                $('#password').val('');
+                ifocus($('#password')[0]);
+                $('#password').val(config.password);
+                $('#password').trigger('keyup');
                 $('#password').trigger('input');
+                iblur($('#password')[0]);
+
                 $('#submit-btn').trigger('click');
             }, 5e3)
             // setTimeout(function() {
